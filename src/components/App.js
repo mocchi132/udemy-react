e62313a8b2f8043468a564954543701555dea80e
mocchi <xxx@xxx.com>
2021-03-23 23:43:21 +0900

    redux connect

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,27 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'
 
-const App = () => (<Counter></Counter>)
+import { increment, decrement } from '../actions'
 
-class Counter extends Component {
-  // stateとは、javaでいうインスタンス変数のこと
-
-  constructor(props){
-    super(props)
-    console.log(this.state)
-    this.state = {count:0}
-  }
-
-  handlePlusButton = () => {
-    const currentCount = this.state.count
-    this.setState({count: currentCount + 1})
-  }
-  handleMinusButton = () => {
-    const currentCount = this.state.count
-    this.setState({count: currentCount - 1})
-  }
-
-  // 初期化のthis.state = {const:0}を使うと、renderが行われず、再描画されない
-  // this.setStateを実行すると、最後にrenderメソッドを実行し、再描画してくれる。
-  // そのため、必ず、setStateをすること
+class App extends Component {
 
   render(){
-    console.log(this.state)
+    const props = this.props
+
     return (
       <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: { props.value }</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value })
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
