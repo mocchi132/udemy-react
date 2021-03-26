944e68a615164207c42b463be40fe11346006b53
mocchi <xxx@xxx.com>
2021-03-26 15:13:52 +0900

    サンプルアプリ作成、redux-thunkインストール

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,14 @@
+import axios from 'axios'
 
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
+export const READ_EVENTS = 'READ_EVENTS'
 
-// export をすると他のコンポーネント側でimportできるようになる
-export const increment = () => ({
-    type: INCREMENT
-})
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
+const QUERYSTRING = '?token=token123'
 
-export const decrement = () => ({
-    type: DECREMENT
-})
+// axiosは非同期処理で、戻り値がpromissなので、asyncとawaitをつける
+export const readEvents = () => async dispatch => {
+  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+  // console.log(response)
+  // dispatchでreducerに、typeとresponseを返してあげる
+  dispatch ({ type: READ_EVENTS,response })
+}
