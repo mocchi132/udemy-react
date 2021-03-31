e3a8fb8878f8af41c3abc6de169023f9471e55c9
mocchi <xxx@xxx.com>
2021-04-01 00:22:51 +0900

    axios redux-thunk APIactionCreater

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import { readEvents } from '../actions'
+import _ from 'lodash'
 
 class EventsIndex extends Component {
 
@@ -9,20 +10,35 @@ class EventsIndex extends Component {
     this.props.readEvents()
   }
 
-  render(){
-    const props = this.props
+  renderEvents(){
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
 
+  render(){
     return (
-      <React.Fragment>
-        <div>value: { props.value }</div>
-        <button onClick={props.increment}>+1</button>
-        <button onClick={props.decrement}>-1</button>
-      </React.Fragment>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
     )
   }
 }
 
-const mapStateToProps = state => ({ })
+const mapStateToProps = state => ({ events: state.events})
 const mapDispatchToProps = ({ readEvents })
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
