c5d01fde1cd5545fe8e19b87c87d3c406b98f783
mocchi <xxx@xxx.com>
2021-04-17 17:27:19 +0900

    new&show material-ui

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import {Field, reduxForm} from 'redux-form';
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 import { postEvent } from '../actions'
 
@@ -18,10 +20,14 @@ class EventsNew extends Component {
     const {input, label, type, meta: {touched, error} } = field
     // 「...変数」演算子は、変数に指定された配列を展開してメソッドに渡してくれる
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   }
 
@@ -34,14 +40,13 @@ class EventsNew extends Component {
     // pritimne：inputになにもなければ、trueになるboolean
     // submitting：submit押下後、trueになるboolean
     const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = { margin: 12 }
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
-        <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-          <Link to="/" >Cancel</Link>
-        </div>
+        <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+        <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />} />
       </form>
     )
   }
--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import {Field, reduxForm} from 'redux-form';
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 import { getEvent, deleteEvent, putEvent } from '../actions'
 
@@ -22,10 +24,14 @@ class EventsShow extends Component {
     const {input, label, type, meta: {touched, error} } = field
     // 「...変数」演算子は、変数に指定された配列を展開してメソッドに渡してくれる
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   }
 
@@ -45,15 +51,14 @@ class EventsShow extends Component {
     // pritimne：inputになにもなければ、trueになるboolean
     // submitting：submit押下後、trueになるboolean
     const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = { margin: 12 }
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
-        <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-          <Link to="/" >Cancel</Link>
-          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
-        </div>
+        <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+        <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />} />
+        <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick} />
       </form>
     )
   }
